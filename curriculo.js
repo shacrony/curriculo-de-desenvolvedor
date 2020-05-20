b994bb79e0a0121568912148a1c4b07143237c3f
Rony novais <61089592+shacrony@users.noreply.github.com>
2020-05-20 14:57:58 -0300

    Add files via upload
    function myResume () {
        const nome = 'Rony Simões Novais'
        const contato = 'https://www.linkedin.com/in/ronynovais/';
    
        let cursos = [
            {
                curso: 'Web Design Completo',
                instituição: 'Origamid',
                periodo: '01.2020 - 02.2020'
            },
            {
                curso: 'manutenção de micros avançada',
                instituição: 'Tecnoponta',
                periodo: '02.2015'
            },
            {
                curso: 'manutenção de placa mãe de pc',
                instituição: 'Tecnoponta',
                periodo: '02.2015'
            }
        ];
    }
    
        let idiomas = [
            {
                idioma: 'Inglês',
                nivel: 'Básico',
            }
        ];
    
        let skills = [
            {
                PHP: 'Básico',
                Javascript: 'Básico',
                CSS: 'Intermediário',
                HTML: 'Intermediário',
                GIT: 'Básico'
            }
        ];
    
        let experiencias = [
            {
                empresa: 'Sagati Celulares',
                cargo: 'Técnico em Infórmatica',
                periodo: '01.2019 - 12.2019'
            }
        ];

---
+++ curriculo.js
@@ -0,0 +1,47 @@
+function myResume () {
+    const nome = 'Rony Simões Novais'
+    const contato = 'https://www.linkedin.com/in/ronynovais/';
+
+    let cursos = [
+        {
+            curso: 'Web Design Completo',
+            instituição: 'Origamid',
+            periodo: '01.2020 - 02.2020'
+        },
+        {
+            curso: 'manutenção de micros avançada',
+            instituição: 'Tecnoponta',
+            periodo: '02.2015'
+        },
+        {
+            curso: 'manutenção de placa mãe de pc',
+            instituição: 'Tecnoponta',
+            periodo: '02.2015'
+        }
+    ];
+}
+
+    let idiomas = [
+        {
+            idioma: 'Inglês',
+            nivel: 'Básico',
+        }
+    ];
+
+    let skills = [
+        {
+            PHP: 'Básico',
+            Javascript: 'Básico',
+            CSS: 'Intermediário',
+            HTML: 'Intermediário',
+            GIT: 'Básico'
+        }
+    ];
+
+    let experiencias = [
+        {
+            empresa: 'Sagati Celulares',
+            cargo: 'Técnico em Infórmatica',
+            periodo: '01.2019 - 12.2019'
+        }
+    ];
